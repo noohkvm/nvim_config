48fb156a02c8b7f5af3f4966f14c2faa30a78ed5
Nooh <nooh@example.com>
2025-02-18 11:02:59 +0530

    add gitsign

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Notes
 install lsp servers (pyright,clangd etc.)
 authenticate with github copilot
+
+# KeyMappings
+Ctrl+b toggle tree
+Ctrl+p command palette
+Ctrl+t terminal
+Ctrl+f format
+gd : go to defnition
+gr : go to references
--- init.lua
+++ init.lua
@@ -6,6 +6,7 @@ vim.keymap.set("n", "<C-f>", function() vim.lsp.buf.format() end, { noremap = tr
 vim.keymap.set("i", "<C-f>", function() vim.lsp.buf.format() end, { noremap = true, silent = true })
 vim.wo.relativenumber = true
 vim.wo.number = true
+vim.o.signcolumn = "yes"
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
